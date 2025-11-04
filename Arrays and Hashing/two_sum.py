801b0dd45cb55754527949e5370c353b09991c83
Deepika <deepikasrigr.1995@gmail.com>
2025-11-04 14:04:41 -0600

    Comitting arrays and hashing problems

--- Arrays and Hashing/two_sum.py
+++ Arrays and Hashing/two_sum.py
@@ -1,18 +1,22 @@
-def two_sum(nums, target):
-    num_map = {}  # value → index
+def two_sum(nums,target):
+    #one-pass hash table
+    dic={}
     for i, num in enumerate(nums):
-        diff = target - num
-        if diff in num_map:
-            return [num_map[diff], i]
-        num_map[num] = i
+        diff = target-num
+        if diff in dic:
+            return dic[diff], i
+        dic[num]=i
     return []
+    
 
-# ------------------------------
 def main():
-    nums = [2, 7, 11, 15]
+    nums=[2,7,9,15]
     target = 9
-    result = two_sum(nums, target)
-    print("Indices:", result)  # Output: [0, 1]
+    result = two_sum(nums,target)
+    print("indices", result)
 
-if __name__ == "__main__":
-    main()
+main()
+
+# TC - O(N)
+# SC - O(N)
+
